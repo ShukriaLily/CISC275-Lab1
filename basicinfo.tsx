015f2f6aa5b31284c7dd759094e0508aa8bde52b
Shukria <shukriam@udel.edu>
2024-09-07 16:19:17 -0400

    Activity Done Activity done!

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -4,5 +4,5 @@ let yourPronouns : String = "She/Her";
 const yourMajor : String = " Cyber Sceurity";
 let classification : String = "Junior";
 let oneExcite : String = "Coding";
-let anyConcerns : String = "N/A";
+let anyConcerns : String = "Nothing yet";
 let funFact : String = "I like dancing!";
